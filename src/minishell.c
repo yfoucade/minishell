ddb2e9219afdd44d4b96b0ba096953ef62a2ba80
yfoucade <yfoucade@student.42.fr>
2022-06-21 18:30:50 +0200

    feat: implement tokenizer. todo: fix memory bug (malloc invalid size unsorted), and invalid pointer for free

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: yfoucade <yfoucade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/19 16:38:44 by yfoucade          #+#    #+#             */
-/*   Updated: 2022/06/21 16:42:02 by yfoucade         ###   ########.fr       */
+/*   Updated: 2022/06/21 18:12:44 by yfoucade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,87 @@
 
 char	g_continue;
 
-char	*add_next_token(t_token *tokens, char *command)
+void	print_tokens(t_token *tokens)
 {
-	
+	int	i;
+
+	i = 0;
+	while (tokens)
+	{
+		printf("token %d: |%s|\n", i++, tokens->token);
+		tokens = tokens->next;
+	}
+}
+
+char	is_meta_except_dollar(char *c)
+{
+	if (*c == ' ' || *c == '\t')
+		return (TRUE);
+	if (*c == '<' || *c == '|' || *c == '>')
+		return (TRUE);
+	if (*c == '\'' || *c == '"') // need to know if it is closed
+		if (*ft_strchr(c + 1, *c) == *c)
+			return (TRUE);
+	return (FALSE);
+}
+
+char	*find_token_end(char *command)
+{
+	char	*closing_quote;
+
+	if (is_blank_chr(*command))
+		return (command + 1);
+	if (*command == '<' || *command == '>')
+		return (command + (command[1] == *command) + 1);
+	if (*command == '|')
+		return (command + (command[1] == *command) + 1);
+	if (*command == '\'' || *command == '"')
+	{
+		closing_quote = ft_strchr(command + 1, *command);
+		if (*closing_quote == *command)
+			return (closing_quote + 1);
+	}
+	while (*command && !is_meta_except_dollar(command))
+		command++;
+	return (command);
+}
+
+void	add_next_token(t_token **tokens, char **command)
+{
+	char	*end;
+	t_token	*tmp;
+	t_token	*new_token;
+
+	while (is_blank_chr(**command))
+		(*command)++;
+	end = find_token_end(*command);
+	new_token = malloc(sizeof(new_token));
+	new_token->next = NULL;
+	// end is the first character that doesn't belong to the token
+	new_token->token = ft_strdup(*command, end - *command - 1);
+	*command = end;
+	if (!*tokens)
+		*tokens = new_token;
+	else
+	{
+		tmp = *tokens;
+		while (tmp->next)
+			tmp = tmp->next;
+		tmp->next = new_token;
+	}
 }
 
 t_token	*tokenize(char	*command)
 {
 	t_token	*res;
 
-	res = malloc(sizeof(res));
+	res = (t_token*)malloc(sizeof(res));
 	res = NULL;
 	while (*command)
-		command = add_next_token(res, command);
+	{
+		add_next_token(&res, &command);
+		printf("tokenize: token added, remaining: |%s|\n", command);
+	}
 	return (res);
 }
 
@@ -41,6 +109,8 @@ unsigned char	execute_command(t_environ *environ)
 	// if (!command)
 	// 	exit_minishell(environ);
 	tokens = tokenize(command);
+	print_tokens(tokens);
+	// free_tokens(tokens);
 	return (0);
 }
 
