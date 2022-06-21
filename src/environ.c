cef24c82199fa84f6fdf2cc576606695542bf666
yfoucade <yfoucade@student.42.fr>
2022-06-21 10:53:51 +0200

    fix: malloc for each field in t_environ.

--- src/environ.c
+++ src/environ.c
@@ -6,7 +6,7 @@
 /*   By: yfoucade <yfoucade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 13:06:03 by yfoucade          #+#    #+#             */
-/*   Updated: 2022/06/21 09:59:32 by yfoucade         ###   ########.fr       */
+/*   Updated: 2022/06/21 10:53:10 by yfoucade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@
 void	init_environ(t_environ *environ)
 {
 	environ->path = malloc(sizeof(environ->path));
+	environ->curr_command = malloc(sizeof(environ->curr_command));
 	environ->curr_command = NULL;
+	environ->last_command = malloc(sizeof(environ->last_command));
 	environ->last_command = NULL;
 	environ->exit_status = 0;
 }
@@ -29,4 +31,6 @@ void	update_environ(t_environ *environ)
 void	free_environ(t_environ *environ)
 {
 	free(environ->path);
+	free(environ->curr_command);
+	free(environ->last_command);
 }
