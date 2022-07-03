6748b382c3cbbdff24e9d094ec11d1d0f3c01df2
Jonas ALLERHAND <jallerha@student.42.fr>
2022-07-03 23:01:17 +0200

    allow to compile multiple sources for testing engine

--- test/utils/compile.py
+++ test/utils/compile.py
@@ -1,6 +1,7 @@
 import subprocess
 import os
 from utils.rnd_string import random_string
+from typing import List
 
 CC = "cc"
 FLAGS = ["-Wall", "-Wextra", "-Werror"]
@@ -12,6 +13,17 @@ def compile(src: str, output_path: str = random_string(12)) -> str:
 	"""
 	try:
 		subprocess.check_call([CC, *FLAGS, src, "-o", output_path])
+	except:
+		return None
+	return output_path
+
+def compile_multiple(srcs: List[str], output_path: str = random_string(12)) -> str:
+	"""
+	Compile multiple C files and return the name of the executable.
+	Returns None if compilation failed.
+	"""
+	try:
+		subprocess.check_call([CC, *FLAGS, *srcs, "-o", output_path])
 	except:
 		return None
 	return output_path
