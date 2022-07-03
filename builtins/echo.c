c6ca1a73fdde58ad7d300dd0bb3da45a4afe60d0
Jonas ALLERHAND <jallerha@student.42.fr>
2022-07-03 21:51:26 +0200

    enhanced implementation of echo

---
+++ builtins/echo.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/03 20:41:52 by jallerha          #+#    #+#             */
+/*   Updated: 2022/07/03 21:32:28 by jallerha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+int	ft_is_flag(char *s)
+{
+	int	i;
+
+	i = 0;
+	if (s[i++] != '-')
+		return (0);
+	while (s[i])
+	{
+		if (s[i] != 'n')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	ft_strlen(char *s)
+{
+	int	n;
+
+	n = 0;
+	while (s[n])
+		n++;
+	return (n);
+}
+
+void	ft_putstr(char *s, int space)
+{
+	write(1, s, ft_strlen(s));
+	if (space)
+		write(1, " ", 1);
+}
+
+void	ft_echo(char **argv)
+{
+	short int	is_string;
+	int			i;
+
+	is_string = 0;
+	i = 0;
+	while (argv[++i])
+	{
+		if ((is_string & 0x1) == 0)
+		{
+			if (ft_is_flag(argv[i]))
+				is_string |= 0x2;
+			else
+			{
+				is_string |= 0x1;
+				i--;
+			}
+		}
+		else if (is_string & 0x1)
+			ft_putstr(argv[i], argv[i + 1] != 0);
+	}
+	if (is_string == 1)
+		ft_putstr("\n", 0);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc > 1)
+		ft_echo(argv);
+	else
+		return (1);
+	return (0);
+}
