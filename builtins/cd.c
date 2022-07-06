ad8da44164ea0f54b5e31752804c8c562c7d40e7
Jonas ALLERHAND <jallerha@student.42.fr>
2022-07-06 21:38:57 +0200

    Refactor and use ft_fprintf to print errors on stderr

--- builtins/cd.c
+++ builtins/cd.c
@@ -3,54 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bacon <bacon@student.42.fr>                +#+  +:+       +#+        */
+/*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/02 01:14:44 by bacon             #+#    #+#             */
-/*   Updated: 2022/07/02 02:01:35 by bacon            ###   ########.fr       */
+/*   Created: 2022/07/02 01:14:44 by jallerha          #+#    #+#             */
+/*   Updated: 2022/07/06 21:38:41 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <errno.h>
 #include <stdio.h>
 #include <unistd.h>
+#include "../neo_libft/includes/printf.h"
 
-// TODO: Remplacer argv par une liste de tokens (ou autre)
-int	ft_change_directory(char **argv)
+// TODO: Remplacer par une liste de tokens (ou autre)
+int	ft_change_directory(char *target)
 {
 	int		err;
 
-	err = chdir(argv[1]);
+	err = chdir(target);
 	if (err != -1)
 		return (0);
 	if (errno == EACCES)
-		puts("cd : Permission denied");
+		ft_fprintf(STDERR_FILENO, "cd : Permission denied\n");
 	else if (errno == ENOENT)
-		puts("cd : No such file or directory");
+		ft_fprintf(STDERR_FILENO, "cd : No such file or directory\n");
 	else if (errno == ENOTDIR)
-		puts("cd : Cannot cd into a file");
+		ft_fprintf(STDERR_FILENO, "cd : Cannot cd into a file\n");
 	else if (errno == EROFS)
-		puts("cd : Read-only file system");
+		ft_fprintf(STDERR_FILENO, "cd : Read-only file system\n");
 	else
-		puts("cd : Unknown error");
+		ft_fprintf(STDERR_FILENO, "cd : Unknown error\n");
 	return (1);
-}
-
-// NOTE: argv[1] est le chemin dans lequel aller, argv[2] permet simplement
-// de print le nouveau cwd pour les tests.
-int	main(int argc, char **argv)
-{
-	char	*cwd;
-	int		ret;
-
-	ret = 0;
-	if (argc >= 2)
-		ret = ft_change_directory(argv);
-	else
-		return (-1);
-	if (argc > 2)
-	{
-		cwd = getcwd(NULL, 0);
-		printf("%s", cwd);
-	}
-	return (ret);
 }
