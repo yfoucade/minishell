0275d1944ef2e755c511b986985968547598f6ee
yfoucade <yohan.foucade@gmail.com>
2022-07-15 03:07:37 +0200

    feat: split wrt unquoted character (use to split composite commands)

--- src/minishell.c
+++ src/minishell.c
@@ -6,44 +6,28 @@
 /*   By: yfoucade <yfoucade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/19 16:38:44 by yfoucade          #+#    #+#             */
-/*   Updated: 2022/07/08 11:41:48 by yfoucade         ###   ########.fr       */
+/*   Updated: 2022/07/15 00:00:10 by yfoucade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char	g_continue;
-char	g_readline;
-
 unsigned char	execute_command(t_environ *environ)
 {
-	char		*command;
-	char		*expanded_command;
-	t_token		*tokens;
-	// t_token		*expanded_tokens;
+	t_str_list	*splitted_command;
 
-	printf("execute_command: received command: |%s|\n", environ->curr_command);
-	command = trim(environ->curr_command);
-	printf("execute_command: trimmed command: |%s|\n", command);
-	// if (!command)
-	// 	exit_minishell(environ);
-	expanded_command = expand(command);
-	printf("execute_command: expand command: |%s|\n", expanded_command);
-	tokens = tokenize(expanded_command);
-	print_tokens(tokens);
-	free(command);
-	free(expanded_command);
-	// free_tokens(tokens);
+	printf("execute_command: received command: ");
+	DEBUG(environ->curr_command);
+	splitted_command = ft_split_unquoted_c(environ->curr_command, '|');
+	print_str_list(splitted_command);
 	return (0);
 }
 
 void	run_shell(t_environ *environ)
 {
-	g_continue = 1;
-	while (g_continue)
+	while (TRUE)
 	{
 		update_environ(environ);
-		g_readline = 1;
 		environ->curr_command = readline(PS1);
 		if (environ->curr_command && *environ->curr_command)
 		{
@@ -58,5 +42,7 @@ void	run_shell(t_environ *environ)
 			// exit_minishell(environ);
 			exit(0);
 		}
+		else
+			free(environ->curr_command);
 	}
 }
