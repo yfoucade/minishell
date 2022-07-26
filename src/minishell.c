fac77a3e27c1fa654da5c640ad1e9b9f5ac3668c
yfoucade <yohan.foucade@gmail.com>
2022-07-26 08:50:18 +0200

    feat: split pipeline

--- src/minishell.c
+++ src/minishell.c
@@ -6,11 +6,43 @@
 /*   By: yfoucade <yfoucade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/19 16:38:44 by yfoucade          #+#    #+#             */
-/*   Updated: 2022/07/15 00:00:10 by yfoucade         ###   ########.fr       */
+/*   Updated: 2022/07/18 02:43:10 by yfoucade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	free_splitted_command(t_str_list *commands)
+{
+	t_str_list *tmp;
+
+	while (commands)
+	{
+		tmp = commands->next;
+		free(commands->str);
+		free(commands);
+		commands = tmp;
+	}
+}
+
+char	is_valid_pipeline(t_str_list *commands)
+{
+	char	*tmp;
+
+	if (!commands)
+		return (FALSE);
+	while (commands)
+	{
+		tmp = commands->str;
+		while (*tmp == ' ' || *tmp == '\t')
+			tmp++;
+		if (!*tmp)
+			return (FALSE);
+		commands = commands->next;
+	}
+	puts("is_valid_pipeline: returning true\n");
+	return (TRUE);
+}
 
 unsigned char	execute_command(t_environ *environ)
 {
@@ -20,6 +52,12 @@ unsigned char	execute_command(t_environ *environ)
 	DEBUG(environ->curr_command);
 	splitted_command = ft_split_unquoted_c(environ->curr_command, '|');
 	print_str_list(splitted_command);
+	if (!is_valid_pipeline(splitted_command))
+	{
+		puts("execute_command: pipeline error\n");
+		// free_splitted_command(splitted_command);
+		return (ERROR);
+	}
 	return (0);
 }
 
