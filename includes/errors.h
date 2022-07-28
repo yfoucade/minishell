1348fc9e5b48a08d68f73be95150d88d94d9de35
Jonas ALLERHAND <jallerha@student.42.fr>
2022-07-28 18:55:53 +0200

    added a placeholder and non-exec file errors

--- includes/errors.h
+++ includes/errors.h
@@ -6,7 +6,7 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 14:30:26 by jallerha          #+#    #+#             */
-/*   Updated: 2022/07/25 14:36:09 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/07/28 13:18:14 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,6 @@
 # define NOT_A_DIR "not a directory"
 # define IS_A_DIR "is a directory"
 # define OPEN_FAILED "failed to open path"
+# define PLACEHOLDER "placeholder error used for testing"
+# define NOT_EXECUTABLE "is a file, but cannot be executed"
 #endif
