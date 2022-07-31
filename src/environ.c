0899c61aca24d9563407b210bf020beb6f8df3f8
yfoucade <yohan.foucade@gmail.com>
2022-07-31 05:47:50 +0200

    refactor: cahnge name environ to environ_

--- src/environ.c
+++ src/environ.c
@@ -6,33 +6,32 @@
 /*   By: yfoucade <yfoucade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 13:06:03 by yfoucade          #+#    #+#             */
-/*   Updated: 2022/07/18 01:18:07 by yfoucade         ###   ########.fr       */
+/*   Updated: 2022/07/31 05:07:52 by yfoucade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 // todo: malloc for each field.
-void	init_environ(t_environ *environ)
+void	init_environ(t_environ *environ_)
 {
-	environ->path = malloc(sizeof(environ->path));
-	environ->argv = malloc(1);
-	environ->argv = NULL;
-	environ->curr_command = malloc(sizeof(environ->curr_command));
-	environ->curr_command = NULL;
-	environ->last_command = malloc(sizeof(environ->last_command));
-	environ->last_command = NULL;
-	environ->exit_status = 0;
+	environ_->path = malloc(sizeof(environ_->path));
+	environ_->argv = NULL;
+	environ_->input = NULL;
+	environ_->pipelines = NULL;
+	environ_->curr_command = NULL;
+	environ_->last_command = NULL;
+	environ_->exit_status = 0;
 }
 
-void	update_environ(t_environ *environ)
+void	update_environ(t_environ *environ_)
 {
-	environ->path = getenv("PATH");
+	environ_->path = getenv("PATH");
 }
 
-void	free_environ(t_environ *environ)
+void	free_environ(t_environ *environ_)
 {
-	free(environ->path);
-	free(environ->curr_command);
-	free(environ->last_command);
+	free(environ_->path);
+	free(environ_->curr_command);
+	free(environ_->last_command);
 }
