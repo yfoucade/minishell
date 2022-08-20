c1d042c72fd7a4589fb4bb2554a77681b9992768
yfoucade <yohan.foucade@gmail.com>
2022-08-20 17:56:25 +0200

    improve input management

--- src/read_input.c
+++ src/read_input.c
@@ -6,7 +6,7 @@
 /*   By: yfoucade <yfoucade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/12 12:40:22 by yfoucade          #+#    #+#             */
-/*   Updated: 2022/08/12 12:40:40 by yfoucade         ###   ########.fr       */
+/*   Updated: 2022/08/19 00:14:40 by yfoucade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,18 @@ char	read_input(t_status *status)
 {
 	status->input = readline(PS1);
 	if (!status->input)
+	{
+		free_status(status);
 		exit(0);
+	}
 	status->pipelines = ft_split_unquoted_c(status->input, '\n');
-	if (!status->input || !status->pipelines)
+	if (!status->pipelines)
 	{
 		free_status(status);
 		perror("minishell: ");
 		exit(0);
 	}
+	free(status->input);
+	status->input = NULL;
 	return (SUCCESS);
 }
