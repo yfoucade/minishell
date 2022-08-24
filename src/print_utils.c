431321bd6c6320bf7f46fd88d7beae206c23ddcb
yfoucade <yohan.foucade@gmail.com>
2022-08-24 03:19:14 +0200

    feat: add ft_putstr() and ft_putfd()

--- src/print_utils.c
+++ src/print_utils.c
@@ -6,7 +6,7 @@
 /*   By: yfoucade <yfoucade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/14 16:49:50 by yfoucade          #+#    #+#             */
-/*   Updated: 2022/07/29 14:48:52 by yfoucade         ###   ########.fr       */
+/*   Updated: 2022/08/23 14:06:59 by yfoucade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,4 +28,14 @@ void	print_str_tab(char **str_tab)
 		printf("%s\n", *str_tab);
 		++str_tab;
 	}
+}
+
+void	ft_putfd(char *str, int fd)
+{
+	write(fd, str, ft_strlen(str));
+}
+
+void	ft_putstr(char *str)
+{
+	ft_putfd(str, 1);
 }
