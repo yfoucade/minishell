d9ab1349ee3d024717154347477afad2566d638d
yfoucade <yohan.foucade@gmail.com>
2022-09-12 23:36:45 +0200

    feat: add print_str_list()

--- libft/print_utils.c
+++ libft/print_utils.c
@@ -6,11 +6,22 @@
 /*   By: yfoucade <yfoucade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/14 16:49:50 by yfoucade          #+#    #+#             */
-/*   Updated: 2022/09/12 16:15:16 by yfoucade         ###   ########.fr       */
+/*   Updated: 2022/09/12 18:11:44 by yfoucade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	print_str_list(t_str_list *lst)
+{
+	if (!lst)
+		return ;
+	while (lst)
+	{
+		printf("%s\n", lst->str);
+		lst = lst->next;
+	}
+}
 
 void	print_str_tab(char **str_tab)
 {
