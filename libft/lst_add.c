7cc6b91187403f27039831d8d9976d55ec9fde6e
yfoucade <yohan.foucade@gmail.com>
2022-09-13 00:21:57 +0200

    refactor: lst_add() now returns a char

--- libft/lst_add.c
+++ libft/lst_add.c
@@ -6,13 +6,13 @@
 /*   By: yfoucade <yfoucade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 16:32:43 by yfoucade          #+#    #+#             */
-/*   Updated: 2022/09/06 16:36:09 by yfoucade         ###   ########.fr       */
+/*   Updated: 2022/09/12 16:20:49 by yfoucade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_str_list	*lst_add(t_str_list **lst, char *str)
+char	lst_add(t_str_list **lst, char *str)
 {
 	return (lst_add_n(lst, str, str + ft_strlen(str) + 1));
 }
