504d608865779deb43b0a66525fc5c3eb462b6c8
yfoucade <yohan.foucade@gmail.com>
2022-09-13 00:28:18 +0200

    refactor: lst_add_n() now returns a char

--- libft/lst_add_n.c
+++ libft/lst_add_n.c
@@ -6,27 +6,35 @@
 /*   By: yfoucade <yfoucade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 16:32:41 by yfoucade          #+#    #+#             */
-/*   Updated: 2022/09/06 16:36:03 by yfoucade         ###   ########.fr       */
+/*   Updated: 2022/09/12 22:51:23 by yfoucade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_str_list	*lst_add_n(t_str_list **lst, char *str, char *end)
+char	lst_add_n(t_str_list **lst, char *str, char *end)
 {
-	t_str_list	*res;
+	t_str_list	*new;
 	t_str_list	*tmp;
 
-	res = malloc(sizeof(*res));
-	res->str = ft_strndup(str, end - str);
-	res->next = NULL;
-	if (!*lst)
-		return (res);
-	tmp = *lst;
-	while (tmp->next)
+	new = malloc(sizeof(*new));
+	if (!new)
+		return (FAILURE);
+	new->str = ft_strndup(str, end - str);
+	if (!new->str)
 	{
-		tmp = tmp->next;
+		free(new);
+		return (FAILURE);
 	}
-	tmp->next = res;
-	return (*lst);
+	new->next = NULL;
+	if (!*lst)
+		*lst = new;
+	else
+	{
+		tmp = *lst;
+		while (tmp->next)
+			tmp = tmp->next;
+		tmp->next = new;
+	}
+	return (SUCCESS);
 }
