c05b05feb33eb9b3d0d47d2b806631efacb3b784
yfoucade <yohan.foucade@gmail.com>
2022-09-15 03:58:21 +0200

    style: norm

--- libft/lst_to_array.c
+++ libft/lst_to_array.c
@@ -6,40 +6,45 @@
 /*   By: yfoucade <yfoucade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 16:32:45 by yfoucade          #+#    #+#             */
-/*   Updated: 2022/09/14 23:15:17 by yfoucade         ###   ########.fr       */
+/*   Updated: 2022/09/15 03:57:35 by yfoucade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+int	lst_size(t_str_list *lst)
+{
+	int	res;
+
+	if (!lst)
+		return (0);
+	res = 0;
+	while (lst)
+	{
+		++res;
+		lst = lst->next;
+	}
+	return (res);
+}
+
 char	**lst_to_array(t_str_list *lst)
 {
 	char		**res;
 	char		**tmp_arr;
-	t_str_list	*tmp_lst;
-	int			count;
 
-	tmp_lst = lst;
-	count = 0;
-	while (tmp_lst)
-	{
-		++count;
-		tmp_lst = tmp_lst->next;
-	}
-	res = malloc(sizeof(char *) * (count + 1));
+	res = malloc(sizeof(char *) * (lst_size(lst) + 1));
 	if (!res)
 		return (NULL);
 	tmp_arr = res;
-	tmp_lst = lst;
-	while (tmp_lst)
+	while (lst)
 	{
-		*tmp_arr++ = ft_strdup(tmp_lst->str);
+		*tmp_arr++ = ft_strdup(lst->str);
 		if (!*(tmp_arr - 1))
 		{
 			free_array(res);
 			return (NULL);
 		}
-		tmp_lst = tmp_lst->next;
+		lst = lst->next;
 	}
 	*tmp_arr = NULL;
 	return (res);
