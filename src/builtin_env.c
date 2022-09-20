8f5767acbf9a4c128a2096cfcce8931d7caaaa8d
Jonas ALLERHAND <jallerha@student.42.fr>
2022-09-20 20:04:05 +0200

    fix: hide empty variables when using env

--- src/builtin_env.c
+++ src/builtin_env.c
@@ -3,25 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_env.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yfoucade <yfoucade@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/24 02:33:12 by yfoucade          #+#    #+#             */
-/*   Updated: 2022/09/15 02:35:24 by yfoucade         ###   ########.fr       */
+/*   Updated: 2022/09/20 20:02:46 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+unsigned int	ft_strnlen(char *s, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (s[i] && i < n)
+		i++;
+	return (i);
+}
+
 unsigned char	env(t_status *status)
 {
 	char	**env;
+	int		i;
 
+	i = -1;
 	env = status->environ;
 	if (!env)
 		return (SUCCESS);
-	while (*env)
+	while (env[++i])
 	{
-		if (ft_putfd(*env++, status->out_fd) || ft_putfd("\n", status->out_fd))
+		if (ft_strnlen(ft_strchr(env[i], '='), 2) < 2)
+			continue;
+		if (ft_putfd(env[i], status->out_fd) || ft_putfd("\n", status->out_fd))
 			return (FAILURE);
 	}
 	return (SUCCESS);
