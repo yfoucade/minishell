92671d6e0064cea3b1d3f26c157318cc38eb902d
yfoucade <yohan.foucade@gmail.com>
2022-09-25 14:11:44 +0200

    fix pwd error msg

--- src/builtin_pwd.c
+++ src/builtin_pwd.c
@@ -6,7 +6,7 @@
 /*   By: yfoucade <yfoucade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/23 13:50:35 by yfoucade          #+#    #+#             */
-/*   Updated: 2022/09/15 02:10:58 by yfoucade         ###   ########.fr       */
+/*   Updated: 2022/09/25 14:09:14 by yfoucade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,11 +28,13 @@ unsigned char	pwd(t_status *status)
 	{
 		status->exit_status = errno;
 		set_error_msg(status, strerror(errno));
+		flush_error_msg(status, NULL);
 	}
 	else
 	{
 		status->exit_status = FAILURE;
 		set_error_msg(status, "pwd: Unknown error\n");
+		flush_error_msg(status, NULL);
 	}
 	return (FAILURE);
 }
