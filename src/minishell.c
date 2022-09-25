99e94ea09a31ef9555296a79f05ea69a31d104fd
yfoucade <yohan.foucade@gmail.com>
2022-09-25 02:53:59 +0200

    refactor: execute every commands in a child process. Wait only at the end

--- src/minishell.c
+++ src/minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yfoucade <yfoucade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/19 16:38:44 by yfoucade          #+#    #+#             */
-/*   Updated: 2022/09/19 12:21:23 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/09/25 02:49:59 by yfoucade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,8 +38,42 @@ void	summon_child(t_status *status)
 	set_exit_status(status);
 }
 
+void	execute(t_status *status)
+{
+	status->child_id = fork();
+	if (!status->child_id)
+	{
+		if (status->out_pipe)
+			close_pipe_end(status->out_pipe, PIPE_OUT);
+		if (status->in_fd != STDIN_FILENO)
+			dup2(status->in_fd, STDIN_FILENO);
+		else if (status->in_pipe)
+			dup2(status->in_pipe[0], STDIN_FILENO);
+		if (status->out_fd != STDOUT_FILENO)
+			dup2(status->out_fd, STDOUT_FILENO);
+		else if (status->out_pipe)
+			dup2(status->out_pipe[1], STDOUT_FILENO);
+		if (status->ft_isatty)
+			waiting_handlers(status);
+		if (status->command->command_type == CMD_BUILTIN)
+			execute_builtin(status);
+		else if (status->command->command_type == CMD_ABS_PATH)
+			execve(status->command->u_command_ref.command_path,
+		status->args, status->environ);
+	}
+}
+
+void	reap_child(t_status *status)
+{
+	if (wait(&status->child_exit_status) == status->child_id)
+		set_exit_status(status);
+}
+
 void	execute_commands(t_status *status)
 {
+	int	n_children;
+
+	n_children = 0;
 	status->curr_command = status->commands;
 	while (status->curr_command && !g_stop_non_int)
 	{
@@ -51,17 +85,19 @@ void	execute_commands(t_status *status)
 			{
 				status->tmp_exit = ft_cmd_not_found(status->args[0]);
 			}
-			else if (status->command->command_type == CMD_BUILTIN)
-				status->tmp_exit = execute_builtin(status);
-			else if (status->command->command_type == CMD_ABS_PATH)
-				summon_child(status);
+			else
+			{
+				++n_children;
+				execute(status);
+			}
 		}
 		flush_error_msg(status, NULL);
 		postprocess_redirections(status);
 		status->curr_command = status->curr_command->next;
 		free_parsed_command(status);
 	}
-	status->exit_status = status->tmp_exit;
+	while (n_children--)
+		reap_child(status);
 	if (g_stop_non_int)
 		free_and_exit(status);
 }
