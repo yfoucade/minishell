a01aa7260ec65b2cb790bedcbd81e157b4773516
yfoucade <yohan.foucade@gmail.com>
2022-09-25 13:51:01 +0200

    fix: cd error messages

--- src/builtin_cd.c
+++ src/builtin_cd.c
@@ -6,7 +6,7 @@
 /*   By: yfoucade <yfoucade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/23 14:08:26 by yfoucade          #+#    #+#             */
-/*   Updated: 2022/09/15 02:15:24 by yfoucade         ###   ########.fr       */
+/*   Updated: 2022/09/25 13:50:11 by yfoucade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,8 @@ unsigned char	cd(t_status *status)
 	ret = array_size(status->args);
 	if (ret == 1)
 	{
-		set_error_msg(status, "minishell: cd: missing directory\n");
+		set_error_msg(status, "cd: missing directory\n");
+		flush_error_msg(status, NULL);
 		return (FAILURE);
 	}
 	ret = chdir(status->args[1]);
@@ -27,7 +28,8 @@ unsigned char	cd(t_status *status)
 	{
 		status->return_value = errno;
 		status->exit_status = errno;
-		perror("cd");
+		perror("minishell: cd");
+		errno = 0;
 		return (FAILURE);
 	}
 	return (SUCCESS);
