7ee9bb45f59961ca9cd522c0e5941e75669be419
yfoucade <yohan.foucade@gmail.com>
2022-09-25 13:31:36 +0200

    fix: builtins outside a pipeline is executed in the main process

--- src/child_process.c
+++ src/child_process.c
@@ -6,7 +6,7 @@
 /*   By: yfoucade <yfoucade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/25 04:19:00 by yfoucade          #+#    #+#             */
-/*   Updated: 2022/09/25 04:21:03 by yfoucade         ###   ########.fr       */
+/*   Updated: 2022/09/25 13:18:49 by yfoucade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +26,15 @@ void	dup_file_descriptors(t_status *status)
 		dup2(status->out_pipe[1], STDOUT_FILENO);
 }
 
-void	execute(t_status *status)
+char	execute(t_status *status)
 {
 	if (status->command->command_type == CMD_BUILTIN
-		&& status->command->u_command_ref.fun_ptr == &ft_exit
 		&& !status->in_pipe && !status->out_pipe)
-		ft_exit(status);
+	{
+		process_output(status);
+		status->exit_status = status->command->u_command_ref.fun_ptr(status);
+		return (0);
+	}
 	status->child_id = fork();
 	if (!status->child_id)
 	{
@@ -45,6 +48,7 @@ void	execute(t_status *status)
 			execve(status->command->u_command_ref.command_path,
 				status->args, status->environ);
 	}
+	return (1);
 }
 
 void	reap_child(t_status *status)
