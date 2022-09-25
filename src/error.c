f288f90a72cb2cc17330f54a6d37e5563cde08a4
yfoucade <yohan.foucade@gmail.com>
2022-09-25 12:59:50 +0200

    fix double error message. Call perror() only if status->error_msg is not set. also print 'minishell: ' before error msg

--- src/error.c
+++ src/error.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   error.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yfoucade <yfoucade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/19 12:05:36 by yfoucade          #+#    #+#             */
-/*   Updated: 2022/09/19 12:46:43 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/09/25 12:55:59 by yfoucade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,18 +41,22 @@ char	set_error_msg(t_status *status, char *str)
 
 void	flush_error_msg(t_status *status, char *str)
 {
-	if (errno)
-		perror("minishell");
-	errno = 0;
 	if (status && status->error_msg)
 	{
+		errno = 0;
+		ft_putfd("minishell: ", STDERR_FILENO);
 		ft_putfd(status->error_msg, STDERR_FILENO);
 		if (status->error_msg[ft_strlen(status->error_msg) - 1] != '\n')
 			ft_putfd("\n", STDERR_FILENO);
 		free(status->error_msg);
 		status->error_msg = NULL;
 	}
-	if (str)
+	else if (errno)
+	{
+		perror("minishell");
+		errno = 0;
+	}
+	else if (str)
 	{
 		ft_putfd("minishell: ", STDERR_FILENO);
 		ft_putfd(str, STDERR_FILENO);
